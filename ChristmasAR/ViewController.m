30059ef9627cb59fe0008d34d2fc41b4016e29b1
Izzywizz <izzywizzz@gmail.com>
2016-11-23 12:17:34 +0000

    Connected previous snowflake animation and flashing red nose into project, also included SnowFalling classes to aid in this

--- ChristmasAR/ViewController.m
+++ ChristmasAR/ViewController.m
@@ -7,24 +7,55 @@
 //
 
 #import "ViewController.h"
+#import "SnowFalling.h"
+
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *santaSnowmanRaindeerImageView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+#pragma mark - UIView Methods
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    NSLog(@"testing");
-    // Do any additional setup after loading the view, typically from a nib.
+    [self generateSnowflakes];
+    [self animateRedRaindeerNose];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Animation Methods
+/**
+ Creating an animiated red nose! This method flicks between two static images of a raindeer with a red nose/ brown nose.
+ The images are animated using the animationImages property
+ */
+-(void) animateRedRaindeerNose  {
+    
+    NSArray *images = [NSArray arrayWithObjects:[UIImage imageNamed:@"characters.png"],[UIImage imageNamed:@"characters-brown-nose.png"], nil];
+    //Then set array to UIImageViews "animationImages"property
+    _santaSnowmanRaindeerImageView.animationImages = images;
+    _santaSnowmanRaindeerImageView.animationDuration = 1;
+    [_santaSnowmanRaindeerImageView startAnimating];
+    //To stop animation just do this
+    //[_imageView stopAnimating];
 }
 
+/**
+ Genreate 3 different types of flakes and add them to the view using the custom framework called 'SnowFalling', the snowFalling class
+ handles the genration/ animation, speed and rotation movement of the snowflakes and the individual properties/ method can be called
+ on from the class. If you wish to add more snowflakes to the view ensure they are named snow-flakes-%d and increase the count on the loop
+ */
+-(void) generateSnowflakes  {
+    for (int i = 1; i < 4; i++) {
+        SnowFalling *snow = [[SnowFalling alloc] initWithView:self.view];
+        // personalize values (optional)
+        snow.numbersOfFlake = 20;
+        snow.directionsOfFlake = SnowFlakeDirectionVertical;
+        snow.imageOfFlake = [UIImage imageNamed:[NSString stringWithFormat:@"snow-flake-%d.png", i]];
+        
+        snow.hidden = NO;
+    }
+}
 
 @end
