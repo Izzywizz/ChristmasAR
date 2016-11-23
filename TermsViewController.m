7a76d34d00fb59bc1f4de246bcb3ef7bdc08bacc
Izzywizz <izzywizzz@gmail.com>
2016-11-23 15:54:57 +0000

    Modified the constraints on elements to include the correct layouts

--- TermsViewController.m
+++ TermsViewController.m
@@ -19,9 +19,18 @@
 #pragma mark - UIView Methods
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     [self.navigationController setNavigationBarHidden:YES];
-    
+    [self setupPlaceholderTextColour];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Helper Functions
+
+-(void) setupPlaceholderTextColour  {
     NSString *placeholderText = @"Enter Email Address";
     if ([_emailAddressTextField respondsToSelector:@selector(setAttributedPlaceholder:)]) {
         UIColor *color = [UIColor whiteColor];
@@ -30,14 +39,7 @@
         NSLog(@"Cannot set placeholder text's color, because deployment target is earlier than iOS 6.0");
         // TODO: Add fall-back code to set placeholder color.
     }
-
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 #pragma mark - Action Methods
 - (IBAction)acceptButtonPressed:(UIButton *)sender {
